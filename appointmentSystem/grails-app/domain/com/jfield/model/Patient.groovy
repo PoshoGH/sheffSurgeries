45879195bea283fe7a287b20554c87b3893dd4a2
PoshoGH <field.joe22@gmail.com>
2023-05-01 16:05:16 +0000

    domaintest

--- appointmentSystem/grails-app/domain/com/jfield/model/Patient.groovy
+++ appointmentSystem/grails-app/domain/com/jfield/model/Patient.groovy
@@ -1,7 +1,22 @@
 package com.jfield.model
 
-class Patient {
+class Patient
+{
+    String PatientID
+    String name
+    
 
-    static constraints = {
+    String toString()
+    {
+        return name
     }
+
+    static constraints =
+    {
+        PatientID unique: true, size: 5..10
+        name size: 5..100
+        course size: 5..100
+    }
+
+    static hasMany = [enrolments: Prescription]
 }
